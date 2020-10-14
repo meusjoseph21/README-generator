383a9e2cdd86a49e7476ad459ea224c32724cf22
JosMuse <no-reply>
2020-10-14 14:04:37 -0600

    questions added to file

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-var fs = require("fs")
+// var fs = require("fs")
 var inquirer = require("inquirer")
-var markdown = require("./utils/generateMarkdown")
+// var markdown = require("./utils/generateMarkdown ")
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "Enter your Github Username: ",
+        name: "github",
+
+    },
+    {
+        type: "input",
+        message: "Enter Email: ",
+        name: "email",
+
+    },
+    {
+        type: "input",
+        message: "Title of your project: ",
+        name: "title"   
+    },
+    {
+        type: "input",
+        message: "Give a Description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Provide a list of contributors: ",
+        name: "contributors"
+    },
+    {
+        type: "list",
+        message: "What type of license are you using?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "none"]
+    },
+    {
+        type: "input",
+        message: "Enter command to run tests: ",
+        name: "tests"
+    }
 
 ];
 
@@ -13,6 +51,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
 
 }
 
