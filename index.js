092fdeaed2118df98afeda0d0df54a41f064abcb
JosMuse <no-reply>
2020-10-14 10:05:23 -0600

    first push bb

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require("fs")
+var inquirer = require("inquirer")
+var markdown = require("./utils/generateMarkdown")
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
